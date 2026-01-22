3750aa36dcbd181e5f1ffcd57bdd741d5041919c
SiamAlSobari <siamqueen52@gmail.com>
2026-01-22 13:32:53 +0700

    feat: add validation and refactor authentication controller with zod integration

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,35 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { loginValidation, registerValidation } from "./auth.validation.js";
+import { UserRepository } from "../user/user.repository.js";
+import { AuthService } from "./auth.service.js";
+import { ApiResponse } from "../../common/utils/response.js";
+import { authMiddleware } from "../../common/middlewares/auth.middleware.js";
+
+const userRepo = new UserRepository();
+const authService = new AuthService(userRepo);
 
 export const authController = new Hono()
-    .post("/register", async (c) => {
-
-    })
-    .post('/login', async (c) => {
-        const payload = await c.req.json();
-        console.log(payload);
-
-        return c.json({
-            message: 'login endpoint hit',
-            payload
-        });
-    })
+    .post(
+        "/register",
+        zValidator("json", registerValidation),
+        async (c) => {
+            const { name, email, password } = c.req.valid("json");
+            const result = await authService.register(name, email, password);
+            return ApiResponse(c, 200, "Berhasil Register", result);
+        })
+    .post(
+        "/login",
+        zValidator("json", loginValidation),
+        async (c) => {
+            const { email, password } = c.req.valid("json");
+            const result = await authService.login(email, password);
+            return ApiResponse(c, 200, "Berhasil Login", result);
+        })
+    .get("/session",
+        authMiddleware,
+        async (c) => {
+            const user = c.get("user");
+            return ApiResponse(c, 200, "Sesi valid", user);
+        }
+    )
